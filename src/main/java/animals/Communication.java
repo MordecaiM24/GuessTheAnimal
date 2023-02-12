112381bb53aeabbdf31e7524e36b65017bfdc192
Ghirmay <abomordecai@gmail.com>
2023-02-12 18:07:45 -0500

    Stage 2 of project; asks for two animals and interprets difference

--- src/main/java/animals/Communication.java
+++ src/main/java/animals/Communication.java
@@ -4,6 +4,48 @@ import java.util.*;
 
 public class Communication {
     static Scanner input = new Scanner(System.in);
+
+    public static String question(String fact){
+        if (fact.contains("can")){
+            return "Can it " + fact.replace("can ", "") + "?";
+        }
+        if (fact.contains("has")){
+            return "Does it have " + fact.replace("has ", "") + "?";
+        }
+        if (fact.contains("is")){
+            return "Is it " + fact.replace("is ", "") + "?";
+        }
+        return "";
+    }
+    public static HashMap<String, String> animalInfo(String sent1, String sent2){
+        HashMap<String, String> animalInfo = new HashMap<>();
+        System.out.println("Specify a fact that distinguishes " + sent1 + " from " + sent2 + ".");
+        System.out.println("The sentence should be of the format: 'It can/has/is ...'.");
+        String fact = input.nextLine();
+        while (!fact.matches("It (can|has|is) .*")){
+            System.out.println("The sentence should be of the format: 'It can/has/is ...'.");
+            System.out.println("The examples of a statement: ");
+            System.out.println("- It can fly \n- It has a horn \n- It is a mammal");
+            System.out.println("Specify a fact that distinguishes " + sent1 + " from " + sent2 + ".");
+            fact = input.nextLine();
+        }
+        fact = fact.replaceFirst("It ", "");
+        fact = fact.replaceAll("[?.!]", "");
+        String question = question(fact);
+        System.out.println("Is it correct for " + sent2 + "?");
+        String factPos = dontStop() ? definite(sent2) : definite(sent1);
+        String factNeg = factPos.equals(definite(sent1)) ? definite(sent2) : definite(sent1);
+
+        animalInfo.put("fact", fact);
+        animalInfo.put("true", factPos);
+        animalInfo.put("false", factNeg);
+        animalInfo.put("question", question);
+
+
+
+        return animalInfo;
+
+    }
     public static void greeting(){
         LocalTime current = LocalTime.now();
         int hour = current.getHour();
@@ -22,9 +64,17 @@ public class Communication {
 
         System.out.println(greeting);
     }
+
+    public static String definite(String indef){
+        if (indef.startsWith("an")){
+            return indef.substring(3);
+        } else {
+            return indef.substring(2);
+        }
+    }
     public static String getAnimal(){
 
-        String sentence = input.nextLine().toLowerCase().replaceFirst("the", "");
+        String sentence = input.nextLine().toLowerCase().replaceFirst("the ", "");
         // Sanitize input; make it lower case and take out "the"
 
         String regex = "\\ban? .*";
@@ -88,23 +138,36 @@ public class Communication {
         int rnd = new Random().nextInt(clarifications.length);
         System.out.println(clarifications[rnd]);
 
-
     }
 
-    public static void dontStop(){
+    public static boolean dontStop(){
         while(true){
             String response = input.nextLine();
 
-            if(Communication.confirmation(response).equals("positive")){
-                System.out.println("You answered: Yes");
-                break;
-            } else if (Communication.confirmation(response).equals("negative")) {
-                System.out.println("You answered: No");
-                break;
+            if(confirmation(response).equals("positive")){
+                return true;
+            } else if (confirmation(response).equals("negative")) {
+                return false;
             } else {
-                Communication.clarification();
-
+                clarification();
             }
         }
     }
+
+    public static String negate(String fact){
+        if(fact.contains("is")){
+            return fact.replaceFirst("is", "isn't");
+        }
+        if(fact.contains("has")){
+            return fact.replaceFirst("has", "doesn't have");
+        }
+        if(fact.contains("can")){
+            return fact.replaceFirst("can", "can't");
+        }
+
+        return fact;
+    }
+
+
 }
+
--- src/main/java/animals/Main.java
+++ src/main/java/animals/Main.java
@@ -1,19 +1,36 @@
 package animals;
-
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
         Communication.greeting();
 
-        System.out.println("\nEnter an animal:");
-        String sent = Communication.getAnimal();
+        System.out.println("\nEnter the first animal:");
+        String sent1 = Communication.getAnimal();
 
-        System.out.println("Is it " + sent + "?");
+        System.out.println("Enter the second animal:");
+        String sent2 = Communication.getAnimal();
 
+        HashMap<String, String> facts = Communication.animalInfo(sent1, sent2);
 
-        Communication.dontStop();
+        System.out.println("I learned the following facts about animals: ");
+        System.out.println(" - The " + facts.get("true") + " " + facts.get("fact"));
+        System.out.println(" - The " + facts.get("false") + " " + Communication.negate(facts.get("fact")));
 
+        System.out.print("I can distinguish these animals by asking the question: \n - ");
+        System.out.println(facts.get("question") + "\n");
         Communication.goodbye();
+
+        System.out.println(facts);
+
+
+
+
+
+
 
     }
 }
